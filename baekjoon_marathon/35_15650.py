11a52b3ae7b8f4034a2e3f9e1453f7595b5771fc
kyungjoon.park <pkj6921@naver.com>
2021-06-21 08:53:33 +0900

    dfs 재귀 문제풀이 업로드

---
+++ baekjoon_marathon/35_15650.py
@@ -0,0 +1,14 @@
+# dfs 개념을 적용!
+
+n, m = list(map(int,input().split()))
+s = []
+def dfs(start):
+  if len(s) == m:
+    print(" ".join(map(str, s)))
+    return
+  for i in range(start, n+1):
+    s.append(i)
+    dfs(i+1)
+    s.pop()
+
+dfs(1)
